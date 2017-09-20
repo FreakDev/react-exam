52f475ccef24320732e2568f195d4f60ae96bd11
Mathias Desloges <freakdev@gmail.com>
2017-09-21 00:56:17 +0200

    remove dependency to uuid node-module

--- src/modules/form/components/Field.js
+++ src/modules/form/components/Field.js
@@ -1,5 +1,4 @@
 import React, { Component, PropTypes } from 'react'
-import uuid from 'uuid/v4'
 
 import './css/Field.css'
 
@@ -74,8 +73,18 @@ class Field extends Component {
     }
 
     render() {
+        function guid() {
+            function s4() {
+                return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+            }
+            return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+                s4() + '-' + s4() + s4() + s4();
+        }
+
         const { name, type, label } = this.props
-        const fieldId = uuid()
+        const fieldId = guid()
         return (
             <div className={ 'field' + (!this.state.isValid ? ' error' : '') }>
                 { label && (<label htmlFor={ fieldId }>{ label }</label>) } : 
